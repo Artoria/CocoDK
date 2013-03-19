865926e4c726603e5d70f92937fb46f975ebe877
Artoria <pochy2008@qq.com>
2013-03-19 21:11:42 +0800

    Update myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -1,16 +1,11 @@
 ﻿class X < GlApp
   def init writer
     super
-    Function.new(writer) do |func|
-       func.start "void orzfly()"
-       Scope.new(func).MessageBox(0, "囧叔好萌!".encode("GBK"), 0, 16).endl
-    end
   end
 
   def gamemain writer
     Input.press?("A").if(writer) do 
        Audio.bgm_play("1.mid").call(self); endl;
-       orzfly; endl;
     end
   end
 
